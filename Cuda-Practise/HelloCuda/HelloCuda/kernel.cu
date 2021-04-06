23d27549e8c08dfcaef74f073e5706d67dc3d3a0
KARTHIK S K <karthikpirus@gmail.com>
2021-04-06 22:35:17 +0530

    cuda

---
+++ Cuda-Practise/HelloCuda/HelloCuda/kernel.cu
@@ -0,0 +1,38 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+__global__ void addArrays(int* a, int* b, int* c)
+{
+	int i = threadIdx.x;
+	c[i] = a[i] + b[i];
+}
+
+void main()
+{
+	const int count = 5;
+	int size = count * sizeof(int);
+	int ha[] = { 1,2,3,4,5};
+	int hb[] = { 10,20,30,40,5};
+	int hc[count];
+
+	int *da, *db, *dc;
+	cudaMalloc(&da, size);
+	cudaMalloc(&db, size);
+	cudaMalloc(&dc, size);
+
+	cudaMemcpy(da, ha, size,cudaMemcpyHostToDevice);
+	cudaMemcpy(db, hb, size, cudaMemcpyHostToDevice);
+	
+	addArrays <<<1,count>>> (da, db, dc);
+	cudaMemcpy(hc, dc, size, cudaMemcpyDeviceToHost);
+
+
+	for (int i = 0; i < count; i++)
+		printf("%d\t", hc[i]);
+
+	getchar();
+
+}
